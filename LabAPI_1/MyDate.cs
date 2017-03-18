245affe931dcace4ca0dc58b5d579870428da90d
OlgaLomina <olgalomina@outlook.com>
2017-03-17 17:13:44 -0700

    commit

---
+++ LabAPI_1/MyDate.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabAPI_1
+{
+    public static class MyDate
+    {
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Checks whether the given year is a leap year
+        /// </summary>
+        /// <param name="year">Year to be verified</param>
+        /// <returns>true if it is a leap year, otherwise false</returns>
+        public static bool IsLeapYear(int year)
+        {
+            bool b = true;
+            b = (year % 4 == 0) && !(year % 100 == 0 && year % 400 != 0);
+            /// return (year % 4 == 0) && !(year % 100 == 0 && year % 400 != 0);
+            return b;
+        }
+
+        /// <summary>
+        /// Determine if this date d is before today's date
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns>true if it is, otherwise false</returns>
+        public static bool IsBefore(DateTime d)
+        {
+            bool b = true;
+            DateTime d_today = DateTime.Today;
+            TimeSpan nDay = d_today - d;
+            int v1 = nDay.Days;
+            //string nCountDay = String.Format("{0:d}", nDay.Days);
+            //int v1 = Convert.ToInt32(nCountDay);
+            if (v1 > 0)
+            {
+                b = true;
+            }
+            else
+            {
+                b = false;
+            }
+            return b;
+        }
+
+        /// <summary>
+        /// Determine if this date d is after today's date
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns>true if it is, otherwise false</returns>
+        public static bool IsAfter(DateTime d)
+        {
+            bool b = true;
+            DateTime d_today = DateTime.Today;
+            TimeSpan nDay = d_today - d;
+            int v1 = nDay.Days;
+            if (v1 < 0)
+            {
+                b = true;
+            }
+            else
+            {
+                b = false;
+            }
+            return b;
+        }
+
+        /// <summary>
+        /// Returns the number of today in the year
+        /// </summary>
+        /// <returns>returns a number 1...366, inclusive, such that this Date is the nth day of its year.</returns>
+        public static int DayInYear()
+        {
+            DateTime dToday = DateTime.Today;
+            int dYear = dToday.Year;
+            string date = "01/01/" + Convert.ToString(dYear);
+            DateTime d = Convert.ToDateTime(date);
+            TimeSpan nDay = dToday - d;
+
+            int k = nDay.Days;
+            return k;
+        }
+
+        #endregion
+    }
+}
---
+++ LabAPI_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabAPI_1
+{
+    class Program
+    {
+        private static DateTime date_v;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input date: ");
+            DateTime date_input = Convert.ToDateTime(Console.ReadLine());
+            int i_year = date_input.Year;
+            if (MyDate.IsLeapYear(i_year))
+            {
+                Console.WriteLine("Year {0} is leap", i_year);
+            }
+            else
+            {
+                Console.WriteLine("Year {0} is not leap", i_year);
+            }
+            
+            if (MyDate.IsBefore(date_input))
+            {
+                Console.Write("Date is before today");
+            }
+            else
+            {
+                Console.Write("Date is not before today");
+            }
+            Console.WriteLine();
+            if (MyDate.IsAfter(date_input))
+            {
+                Console.Write("Date is after today");
+            }
+            else
+            {
+                Console.Write("Date is not after today");
+            }
+            Console.WriteLine();
+            DateTime dToday = DateTime.Today;
+            i_year = dToday.Year;
+            int j = MyDate.DayInYear();
+            Console.WriteLine("Date is the {0}th day of {1} year.", j, i_year);
+
+        }
+    }
+}
